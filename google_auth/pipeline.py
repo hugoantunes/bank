3ff917085cbcb5bda913c88641c3a038e5ec7e60
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-08-14 23:46:41 -0300

    improve pipeline functions:  - just save UserAdmin when is necessary  - just add permission when is necessary

--- google_auth/pipeline.py
+++ google_auth/pipeline.py
@@ -6,8 +6,9 @@ from bank_user.models import User
 
 def set_access(backend, details, response, *args, **kwargs):
     user = kwargs.get("user")
+    if user.is_staff and user.is_active:
+        return
     user.is_staff = True
-    user.is_admin = True
     user.is_active = True
     user.save()
 
@@ -15,5 +16,7 @@ def set_access(backend, details, response, *args, **kwargs):
 def add_bank_user_permission(backend, details, response, *args, **kwargs):
     user = kwargs.get("user")
     content_type = ContentType.objects.get_for_model(User)
-    for permission in Permission.objects.filter(content_type=content_type):
-        user.user_permissions.add(permission)
+    permissions = Permission.objects.filter(content_type=content_type)
+    if not user.has_module_perms(content_type.app_label):
+        for permission in permissions:
+            user.user_permissions.add(permission)
