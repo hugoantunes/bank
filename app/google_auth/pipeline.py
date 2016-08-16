45c4312ea7e5e656d949b2899733841bc5f7fcc6
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-08-15 22:53:49 -0300

    tests for custom pipelines

--- app/google_auth/pipeline.py
+++ app/google_auth/pipeline.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from bank_user.models import User
 
 
-def set_access(backend, details, response, *args, **kwargs):
+def set_access(*args, **kwargs):
     user = kwargs.get("user")
     if user.is_staff and user.is_active:
         return
@@ -13,10 +13,10 @@ def set_access(backend, details, response, *args, **kwargs):
     user.save()
 
 
-def add_bank_user_permission(backend, details, response, *args, **kwargs):
+def add_bank_user_permission(*args, **kwargs):
     user = kwargs.get("user")
     content_type = ContentType.objects.get_for_model(User)
     permissions = Permission.objects.filter(content_type=content_type)
-    if not user.has_module_perms(content_type.app_label):
+    if not set(user.user_permissions.all()) == set(content_type.permission_set.all()):
         for permission in permissions:
             user.user_permissions.add(permission)
--- app/google_auth/tests.py
+++ app/google_auth/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User as AdminUser
 
-# Create your tests here.
+from google_auth.pipeline import set_access, add_bank_user_permission
+from bank_user.models import User
+
+
+class PipelineTest(TestCase):
+    def setUp(self):
+        self.admin_user = AdminUser.objects.create_user(
+            username='admin',
+            email='admin@…',
+            password='top_secret'
+        )
+        self.admin_user.is_staff = False
+        self.admin_user.is_active = False
+        self.admin_user.save()
+
+    def test_set_access(self):
+        set_access(user=self.admin_user)
+
+        self.admin_user = AdminUser.objects.get()
+        self.assertTrue(self.admin_user.is_staff)
+        self.assertTrue(self.admin_user.is_active)
+
+    def test_add_bank_user_permission(self):
+        content_type = ContentType.objects.get_for_model(User)
+        add_bank_user_permission(user=self.admin_user)
+
+        self.admin_user = AdminUser.objects.get()
+        self.assertTrue(
+            set(self.admin_user.user_permissions.all()) == set(content_type.permission_set.all())
+        )
