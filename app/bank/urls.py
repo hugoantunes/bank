320c1b7d87437df1778bf1e85e72cf6a861e293a
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-08-15 21:47:06 -0300

    redirect / to /admin

--- app/bank/urls.py
+++ app/bank/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 from google_auth.views import login
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/login/$', login),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls), name='admin'),
+    url(r'^$', RedirectView.as_view(url='/admin')),
     url('', include('social.apps.django_app.urls', namespace='social')),
 ]
