e794a7e415eb1ca783f37338d75c0ec54b14d9f6
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-08-16 00:28:30 -0300

    correct way to export enviroment variables

--- provision/google_auth.sh
+++ provision/google_auth.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+BANK_GOOGLE_OAUTH2_KEY=''
+BANK_GOOGLE_OAUTH2_SECRET=''
 
-export BANK_GOOGLE_OAUTH2_KEY=""
-export BANK_GOOGLE_OAUTH2_SECRET=""
+echo "##########################################################################################"
+echo "                              export google auth keys                                     "
+su - vagrant -c "echo export BANK_GOOGLE_OAUTH2_KEY=$BANK_GOOGLE_OAUTH2_KEY >> ~/.profile"
+su - vagrant -c "echo export BANK_GOOGLE_OAUTH2_SECRET=$BANK_GOOGLE_OAUTH2_SECRET >> ~/.profile"
+su - vagrant -c "cat ~/.profile"
+echo "##########################################################################################"
